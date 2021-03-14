80920a83052fee648c7938c606a5f07af3c3056b
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-14 18:19:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello W!");
+
+
+            string frase, cebolinha;
+
+            Console.WriteLine("Digite uma frase:");
+            frase = Console.ReadLine();
+            cebolinha = frase.Replace("r", "l").Replace("R", "L");
+            Console.WriteLine($"Texto Cebolinha: {cebolinha}");
+        }
+    }
+}
