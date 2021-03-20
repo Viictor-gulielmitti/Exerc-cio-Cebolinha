b1ae44e71be93971fb7b3584752c3ca97da6469f
Diego Neri <diego@diegoneri.com>
2021-03-19 22:26:08 -0300

    Adiciona substituição de duplos e caixa alta
    Inclusão de replaces para rr e variantes em letras maiúsculas (caixa alta)

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Cebolinha
 
             Console.WriteLine("Digite uma frase:");
             frase = Console.ReadLine();
-            cebolinha = frase.Replace("r", "l").Replace("R", "L");
+            cebolinha = frase.Replace("rr","l").Replace("rR","l").Replace("RR","L").Replace("Rr","L").Replace("r","l").Replace("R","L");
             Console.WriteLine($"Texto Cebolinha: {cebolinha}");
         }
     }
